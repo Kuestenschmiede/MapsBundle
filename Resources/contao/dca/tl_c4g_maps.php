d6bda8eaa4e55ae45f54efea42bf916cd254b3e8
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-10-16 14:33:28 +0200

    fix subpalette

--- Resources/contao/dca/tl_c4g_maps.php
+++ Resources/contao/dca/tl_c4g_maps.php
@@ -1385,7 +1385,7 @@ class tl_c4g_maps extends Backend
             }
 
             $GLOBALS['TL_DCA']['tl_c4g_maps']['subpalettes']['is_map'] =
-                'width,height,margin,show_locations,'.$calcExtentFields.'center_geox,center_geoy,zoom,geolocation,'.$geolocationFields.'restrict_area,'.$restrictAreaFields.',include_sublocations;';
+                'width,height,margin,show_locations,'.$calcExtentFields.'center_geox,center_geoy,zoom,geolocation,'.$geolocationFields.'restrict_area,'.$restrictAreaFields.',include_sublocations';
 
             if ($GLOBALS['con4gis']['forum']['installed']) {
                 $GLOBALS['TL_DCA']['tl_c4g_maps']['palettes']['overpass'] = str_replace('popup_info,routing_to', 'popup_info,popup_extend,routing_to', $GLOBALS['TL_DCA']['tl_c4g_maps']['palettes']['overpass']);
