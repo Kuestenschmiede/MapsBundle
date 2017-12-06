abb0bd8f5792f14d5ee8a22593aecbf5dc0a6e5f
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-12-06 16:40:28 +0100

    ol 4.6.0

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -14,7 +14,7 @@
 
 $GLOBALS['con4gis']['maps']['installed'] = true;
 $GLOBALS['con4gis']['maps']['debug'] = false;
-$GLOBALS['con4gis']['maps']['ol-version'] = '4.5.0';
+$GLOBALS['con4gis']['maps']['ol-version'] = '4.6.0';
 
 /**
  * Sourcetable definition
