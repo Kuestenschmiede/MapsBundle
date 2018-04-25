e27a2a607f196a4259fc58a05c06a8bc972dbb16
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-04-25 18:04:30 +0200

    fix dca default values

--- Resources/contao/dca/tl_c4g_map_layer_content.php
+++ Resources/contao/dca/tl_c4g_map_layer_content.php
@@ -47,98 +47,122 @@ $GLOBALS['TL_DCA']['tl_c4g_map_layer_content'] = array
         ),
         'pid' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'type' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'format' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'origType' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'locstyle' => array(
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
         'datatype' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'geotype' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'geox' => array
         (
-            'sql'                     => "varchar(20)"
+            'sql'                     => "varchar(20) default ''",
+            'default'                 => ''
         ),
         'geoy' => array
         (
-            'sql'                     => "varchar(20)"
+            'sql'                     => "varchar(20) default ''",
+            'default'                 => ''
         ),
         'projection' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'popup_content' => array
         (
-            'sql'                     => "text default ''"
+            'sql'                     => "text default ''",
+            'default'                 => ''
         ),
         'popup_routing_link' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
         'popup_async' => array
         (
-            'sql'                     => "varchar(100)"
+            'sql'                     => "varchar(100) default ''",
+            'default'                 => ''
         ),
         'tooltip' => array
         (
-            'sql'                     => "text"
+            'sql'                     => "text default ''",
+            'default'                 => ''
         ),
         'tooltip_length' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
         'label' => array
         (
-            'sql'                     => "text "
+            'sql'                     => "text  default ''",
+            'default'                 => ''
         ),
         'cluster_distance' => array
         (
-            'sql'                     => "varchar(100) "
+            'sql'                     => "varchar(100)  default ''",
+            'default'                 => ''
         ),
         'cluster_fillcolor' => array
         (
-           'sql'                     => "varchar(6)"
+            'sql'                     => "varchar(6) default ''",
+            'default'                 => ''
         ),
         'cluster_fontcolor' => array
         (
-             'sql'                     => "varchar(6)"
+            'sql'                     => "varchar(6) default ''",
+            'default'                 => ''
         ),
         'cluster_zoom' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => ''
         ),
         'cluster_popup' => array
         (
-            'sql'                     => "char(1)"
+            'sql'                     => "char(1) default ''",
+            'default'                 => false
         ),
         'loc_linkurl' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
         'hover_location' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
         'hover_style' => array
         (
-            'sql'                     => "int(10)"
+            'sql'                     => "int(10) default '0'",
+            'default'                 => '0'
         ),
     )
 );
