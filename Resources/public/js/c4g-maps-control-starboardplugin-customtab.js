4773833477113389eb692a74c4721b4284ec083c
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-06-22 08:28:54 +0200

    add font awesome 5.1

--- Resources/public/js/c4g-maps-control-starboardplugin-customtab.js
+++ Resources/public/js/c4g-maps-control-starboardplugin-customtab.js
@@ -334,12 +334,16 @@ this.c4g.maps.control.starboardplugin = this.c4g.maps.control.starboardplugin ||
             } // end of for-loop
           // Starboard Filter
           if (this.starboard.options.filter) {
+              var link = document.createElement('link');
+              link.href = "bundles/con4giscore/vendor/fontawesome/css/all.css";
+              link.rel="stylesheet";
+
               var dv = document.createElement('div');
               dv.className = "c4g-starboard-filter c4g-content-select";
               var filter = document.createElement('input');
               filter.type = 'text';
               // filter.className = 'c4g-starboard-filter';
-              filter.style = "font-family:'Font Awesome 5 Free'!important;";
+              filter.style = "font-family:'Font Awesome 5 Free'!important; font-weight: 900;";
               filter.placeholder = "&#xf00d";  // Font Awesome
               dv.appendChild(filter);
               filter.onkeyup = function () {
