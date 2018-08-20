5ccfd9f52254f626ef7e5dd5a0feed569b452eaf
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-08-20 11:54:30 +0200

    remove mandatory from editor icon size

--- Resources/contao/dca/tl_c4g_map_locstyles.php
+++ Resources/contao/dca/tl_c4g_map_locstyles.php
@@ -551,7 +551,7 @@ $GLOBALS['TL_DCA']['tl_c4g_map_locstyles'] = array
             'default'                 => array('32','32'),
             'inputType'               => 'imageSize',
             'options'                  => $imageSizes,
-            'eval'                    => array('rgxp'=>'digit', 'tl_class'=>'long', 'mandatory'=>true),
+            'eval'                    => array('rgxp'=>'digit', 'tl_class'=>'long', 'mandatory'=>false),
             'sql'                     => "varchar(100) NOT NULL default ''"
         ),
         'editor_sort' => array
