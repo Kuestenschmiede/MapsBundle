f0e5d8c1cf69a785c2cb6384fe221c8179e111fa
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-08-21 14:08:29 +0200

    undefined check

--- Resources/public/js/c4g-layer-controller.js
+++ Resources/public/js/c4g-layer-controller.js
@@ -476,12 +476,12 @@ class C4gLayerController{
                   // calculate bubble-offset
                   iconOffset = [0, 0];
                   if (style[0]) {
-                    if (typeof style[0].getImage() && typeof style[0].getImage().getRadius === "function") {
+                    if (typeof style[0].getImage() && style[0].getImage().getRadius && typeof style[0].getImage().getRadius === "function") {
                       radius = parseInt(style[0].getImage().getRadius(), 10);
                       if (radius) {
                         iconOffset = [0, radius];
                       }
-                    } else if (style[0].getImage() && typeof style[0].getImage().getAnchor === "function") {
+                    } else if (style[0].getImage() && style[0].getImage().getAnchor && typeof style[0].getImage().getAnchor === "function") {
                       iconOffset = style[0].getImage().getAnchor() || [0, 0];
                     }
                   }
