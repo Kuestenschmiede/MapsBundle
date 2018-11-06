47fae04e84f07149cc1be2b312614841a6da2413
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-11-06 18:19:43 +0100

    ol-mapbox-style 3.2.1

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -16,7 +16,7 @@ $GLOBALS['con4gis']['maps']['installed'] = true;
 $GLOBALS['con4gis']['maps']['debug'] = false;
 $GLOBALS['con4gis']['maps']['ol-version'] = 'v5.2.0';
 $GLOBALS['con4gis']['maps']['ol-cesium-version'] = 'v2.4.0';
-$GLOBALS['con4gis']['maps']['olms-version'] = 'v3.0.1';
+$GLOBALS['con4gis']['maps']['olms-version'] = 'v3.2.1';
 $GLOBALS['con4gis']['maps']['ol-ext'] = 'v3.0.2';
 
 /**
