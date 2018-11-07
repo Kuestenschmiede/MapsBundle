e4ec7783e64bad3fb21a5c5fcd63cbc0181a6a74
coastforge-cro <claudio.ross@kuestenschmiede.de>
2018-11-07 11:52:27 +0100

    Remove unsupported ol-debug property

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -13,7 +13,6 @@
 
 
 $GLOBALS['con4gis']['maps']['installed'] = true;
-$GLOBALS['con4gis']['maps']['debug'] = false;
 $GLOBALS['con4gis']['maps']['ol-version'] = 'v5.2.0';
 $GLOBALS['con4gis']['maps']['ol-cesium-version'] = 'v2.2.2';
 $GLOBALS['con4gis']['maps']['olms-version'] = 'v3.0.1';
