5b9f0ad0479ca0664551d66264efa1ddbbbc24d2
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2019-03-11 19:00:41 +0100

    updated settings

--- Classes/Contao/Callbacks/TlC4gMapSettings.php
+++ Classes/Contao/Callbacks/TlC4gMapSettings.php
@@ -17,19 +17,12 @@ class TlC4gMapSettings extends \Backend
     public function addDefaultTables($dc) {
 
         // check wether default-data should be set
-        $strSelectSettings = "SELECT * FROM tl_c4g_settings";
-        $strSettings = $this->Database->prepare($strSelectSettings)->execute()->fetchAssoc();
-        $arrSettings = unserialize($strSettings['defaultData']);
-        if ($arrSettings[0]) {
-
-            // set settings so this function is not triggered each time
-            $arrSettings[0] = 0;
-            $strSettings = serialize($arrSettings);
-            $strUpdate = "UPDATE tl_c4g_settings SET defaultData = ?";
-            $this->Database->prepare($strUpdate)->execute($strSettings);
-
+        $strSelectTables = "SELECT * FROM tl_c4g_map_tables";
+        $strTables = $this->Database->prepare($strSelectTables)->execute()->fetchAssoc();
+        if (!$strTables) {
             // set default-data for tl_calendar_events
             $this->translateArrToDB('tl_calendar_events', [
+                'name'          => 'Events',
                 'ptable'        => 'tl_calendar',
                 'ptable_option' => 'title',
                 'geox'          => 'c4g_loc_geox',
@@ -46,6 +39,7 @@ class TlC4gMapSettings extends \Backend
 
             // set default data for tl_content
             $this->translateArrToDB('tl_content', [
+                'name'          => 'Content',
                 'geox'          => 'c4g_loc_geox',
                 'geoy'          => 'c4g_loc_geoy',
                 'label'         => 'c4g_loc_label',
@@ -56,6 +50,7 @@ class TlC4gMapSettings extends \Backend
 
             // set default data for tl_member
             $this->translateArrToDB('tl_member', [
+                'name'          => 'Member',
                 'geox'          => 'c4g_loc_geox',
                 'geoy'          => 'c4g_loc_geoy',
                 'label'         => 'c4g_loc_label',
