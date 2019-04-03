18a920db213c3f9533b81abfc24afb9b08474aff
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2019-04-03 13:25:46 +0200

    hotfix

--- Resources/public/js/c4g-maps-main.js
+++ Resources/public/js/c4g-maps-main.js
@@ -12,12 +12,6 @@
 
 import {MapController} from "./c4g-maps"
 
-jQuery(document).ready(function() {
-  if (typeof window.mapData !== "undefined") {
-    window.initMaps(window.mapData);
-  }
-});
-
 window.initMaps = function(mapData) {
   for (let key in mapData) {
     if (mapData.hasOwnProperty(key)) {
@@ -25,3 +19,9 @@ window.initMaps = function(mapData) {
     }
   }
 };
+jQuery(document).ready(function() {
+  if (typeof window.mapData !== "undefined") {
+    window.initMaps(window.mapData);
+  }
+});
+
