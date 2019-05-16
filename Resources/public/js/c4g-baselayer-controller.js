d519c56c2fcff6661fbe7c93e000eeb2d171ee6c
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2019-05-16 16:53:29 +0200

    add semicolon

--- Resources/public/js/c4g-baselayer-controller.js
+++ Resources/public/js/c4g-baselayer-controller.js
@@ -482,7 +482,7 @@ export class C4gBaselayerController {
         if (layer) {
           let showLayer = false;
           if (layer.activeForBaselayers == "all") {
-            continue
+            continue;
           }
           else {
             for (let activeBaselayerId in layer.activeForBaselayers) {
