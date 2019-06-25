03474e85c4bbe047d4958c0ea148d3920a08b688
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-06-25 13:17:37 +0200

    Set click and touchstart listeners passive

--- Resources/public/js/c4g-maps-control-grid.js
+++ Resources/public/js/c4g-maps-control-grid.js
@@ -96,8 +96,8 @@ export class Grid extends Control {
       element.appendChild(button);
 
       // set onClick to the toggle-function
-      button.addEventListener('click', toggle, false);
-      button.addEventListener('touchstart', toggle, false);
+      button.addEventListener('click', toggle, {useCapture: false, passive: true});
+      button.addEventListener('touchstart', toggle, {useCapture: false, passive: true});
     }
 
     // inheritance-stuff
--- Resources/public/js/c4g-maps-control-position.js
+++ Resources/public/js/c4g-maps-control-position.js
@@ -91,8 +91,8 @@ export class Position extends Control {
       element.appendChild(button);
 
       // set onClick to the toggle-function
-      button.addEventListener('click', toggle, false);
-      button.addEventListener('touchstart', toggle, false);
+      button.addEventListener('click', toggle, {useCapture: false, passive: true});
+      button.addEventListener('touchstart', toggle, {useCapture: false, passive: true});
     }
 
     // inheritance-stuff
