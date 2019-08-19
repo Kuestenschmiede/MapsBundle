d8d03edd461f04265756160e1d46734682fc8fdd
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-08-19 14:58:49 +0200

    Fix purge cache methods

--- Classes/Caches/C4GMapsAutomator.php
+++ Classes/Caches/C4GMapsAutomator.php
@@ -15,34 +15,36 @@ namespace con4gis\MapsBundle\Classes\Caches;
 
 
 use con4gis\CoreBundle\Resources\contao\classes\C4GAutomator;
+use Contao\System;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class C4GMapsAutomator extends C4GAutomator
 {
     /**
      * Purge the con4gis cache for the layerService.
      */
-    public static function purgeLayerApiCache()
+    public function purgeLayerApiCache()
     {
-
-        C4GLayerApiCache::getInstance()->clearCache();
+        
+        C4GLayerApiCache::getInstance(System::getContainer())->clearCache();
         // Add a log entry
     }
 
     /**
      * Purge the con4gis cache for the baselayerService.
      */
-    public static function purgeBaselayerApiCache()
+    public function purgeBaselayerApiCache()
     {
-        C4GBaselayerApiCache::getInstance()->clearCache();
+        C4GBaselayerApiCache::getInstance(System::getContainer())->clearCache();
         // Add a log entry
     }
 
     /**
      * Purge the con4gis cache for the locationstyleService.
      */
-    public static function purgeLocationstyleApiCache()
+    public function purgeLocationstyleApiCache()
     {
-        C4GLocationstyleApiCache::getInstance()->clearCache();
+        C4GLocationstyleApiCache::getInstance(System::getContainer())->clearCache();
         // Add a log entry
     }
 }
