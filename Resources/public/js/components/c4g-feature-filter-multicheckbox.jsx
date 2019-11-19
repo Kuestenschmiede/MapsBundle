e2387a225cd9f82de395303ac9b3497282802d9d
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2019-11-19 14:28:18 +0100

    first draft multicheckbox-filter

---
+++ Resources/public/js/components/c4g-feature-filter-multicheckbox.jsx
@@ -0,0 +1,47 @@
+/*
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package   	con4gis
+ * @version        6
+ * @author  	    con4gis contributors (see "authors.txt")
+ * @license 	    LGPL-3.0-or-later
+ * @copyright 	Küstenschmiede GmbH Software & Design
+ * @link              https://www.con4gis.org
+ *
+ */
+
+import React, { Component } from "react";
+import {utils} from "../c4g-maps-utils"
+import {FeatureFilterMultiCheckboxItem} from "./c4g-feature-multicheckbox-item.jsx";
+
+export class FeatureFilterMultiCheckbox extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const scope = this;
+        let form = null;
+        let className;
+        if (this.props.open) {
+            className = "c4g-open";
+            form =
+                <form>
+                    {this.props.feature.filters.map((feature, index) => {
+                        return <FeatureFilterMultiCheckboxItem feature={feature} parentId={this.props.id} checked={this.props.checkedItems} filterLayers={this.props.filterLayers} key={index}/>
+                    })}
+                </form>
+        }
+        else {
+            className = "c4g-close";
+        }
+        className += " fi_" + utils.removeUmlauts(this.props.feature.name);
+        let liClass =  "c4g-item-checked";
+        return (
+            <li className={liClass}>
+                <strong className={className} onMouseUp={(evt) => {this.props.setOpen(this.props.id); evt.stopPropagation(); evt.preventDefault();}}><span/>{this.props.feature.name}</strong>
+                {form}
+            </li>
+        );
+    }
+}
