c34c4bffe058b06dbc217847f04f77934c9fc929
coastforge_mei <matthias.eilers@kuestenschmiede.de>
2020-01-24 10:32:04 +0100

    removed location, protection, publish settings and checkbox limit from map element

--- Resources/contao/dca/tl_c4g_maps.php
+++ Resources/contao/dca/tl_c4g_maps.php
@@ -138,11 +138,7 @@ $GLOBALS['TL_DCA']['tl_c4g_maps'] =
         '__selector__'                => ['is_map', 'profile','location_type', 'tab_source', 'show_locations', 'popup_extend', 'protect_element', 'use_specialprofile', 'cluster_locations', 'split_geojson'],
         'default'                     => '{general_legend},name,profile,profile_mobile;'.
                                          '{map_legend},is_map;'.
-                                         '{location_legend},location_type,data_layername,hide_child,initial_opened,exemptFromFilter,filterByBaseLayer,data_hidelayer,cssClass,zoom_locations, hover_location,loc_minzoom,loc_maxzoom;'.
-                                         '{protection_legend:hide},protect_element;'.
-                                         '{publish_legend:hide},published,publishStart,publishStop;'.
-                                         '{expert_legend:hide},use_specialprofile;'.
-                                         '{backend_legend:hide},be_optimize_checkboxes_limit;',
+                                         '{expert_legend:hide},use_specialprofile;',
         'single'                      => '{general_legend},name;'.
                                          '{map_legend},is_map;'.
                                          '{location_legend},location_type,data_layername,initial_opened,exemptFromFilter,filterByBaseLayer,hide_child,data_hidelayer,loc_geox,loc_geoy,locstyle,zIndex,loc_only_in_parent,loc_label,tooltip, tooltip_length,enablePopup,showPopupOnActive,popup_info,loc_linkurl,loc_onclick_zoomto,loc_minzoom,loc_maxzoom,zoom_locations, hover_location,hide_when_in_tab,hideInStarboard,cssClass;'.
