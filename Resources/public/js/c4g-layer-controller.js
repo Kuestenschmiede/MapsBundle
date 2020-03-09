7adeded225c89829a7a31d3c6af7a888d8ca55ff
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2020-03-09 16:47:06 +0100

    prevent missing coordinates from crashing starboard

--- Resources/public/js/c4g-layer-controller.js
+++ Resources/public/js/c4g-layer-controller.js
@@ -792,7 +792,7 @@ export class BetterLayerController {
               return false;
             }
           }
-          else {
+          else if (contentData.properties){
             format = new olFormat[layer.content[contentId].type]({
               featureProjection: featureProjection,
               dataProjection: contentData.properties.projection
