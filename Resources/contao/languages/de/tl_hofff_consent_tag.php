859150ecfaaee73ed6e8970c8ce892e7dabe4102
Friedrich Schute <friedrich.schute@kuestenschmiede.de>
2020-12-15 17:32:52 +0100

    first custom baselayer with consent

---
+++ Resources/contao/languages/de/tl_hofff_consent_tag.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package   	con4gis
+ * @version    7
+ * @author  	    con4gis contributors (see "authors.txt")
+ * @license 	    LGPL-3.0-or-later
+ * @copyright 	Küstenschmiede GmbH Software & Design
+ * @link              https://www.con4gis.org
+ *
+ */
+
+
+$strName = "tl_hofff_consent_tag";
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG'][$strName]['types']['mapbox'] = ["Mapbox-Basiskarten", "Mapbox"];
