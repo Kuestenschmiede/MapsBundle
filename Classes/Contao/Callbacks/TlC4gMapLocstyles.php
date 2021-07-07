494de1723d403fd289be8ec464ac23219d597ed9
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-07-07 16:39:03 +0200

    Fixed prod build

--- Classes/Contao/Callbacks/TlC4gMapLocstyles.php
+++ Classes/Contao/Callbacks/TlC4gMapLocstyles.php
@@ -52,23 +52,21 @@ class TlC4gMapLocstyles extends Backend
     }
     public function setSizes($varValue, DataContainer $dc)
     {
-
         if ($dc->activeRecord->styletype == 'cust_icon') {
             $icon_src = $dc->activeRecord->icon_src;
             $icon_size = unserialize($dc->activeRecord->icon_size);
             $id = $dc->id;
             if ($icon_src && !($icon_size && $icon_size[0])) {
-                if (\Validator::isUuid($icon_src))
-                {
+                if (\Validator::isUuid($icon_src)) {
                     $iconSrc = \FilesModel::findByUuid($icon_src);
                     list($width, $height, $type, $attr) = getimagesize($iconSrc->path);
-                    $icon_size = 'a:3:{i:0;s:'.strlen($width).':"'.$width.'";i:1;s:'.strlen($height).':"'.$height.'";i:2;s:2:"px";}';
+                    $icon_size = 'a:3:{i:0;s:' . strlen($width) . ':"' . $width . '";i:1;s:' . strlen($height) . ':"' . $height . '";i:2;s:2:"px";}';
                     $dc->activeRecord->icon_size = $icon_size;
-                    $offsetWidth = '-'.($width / 2);
-                    $offsetHeight = '-'.($height / 2);
-                    $icon_offset = 'a:3:{i:0;s:'.strlen($offsetWidth).':"'.$offsetWidth.'";i:1;s:'.strlen($offsetHeight).':"'.$offsetHeight.'";i:2;s:2:"px";}';
+                    $offsetWidth = '-' . ($width / 2);
+                    $offsetHeight = '-' . ($height / 2);
+                    $icon_offset = 'a:3:{i:0;s:' . strlen($offsetWidth) . ':"' . $offsetWidth . '";i:1;s:' . strlen($offsetHeight) . ':"' . $offsetHeight . '";i:2;s:2:"px";}';
                     $dc->activeRecord->icon_offset = $icon_offset;
-                    $this->Database->prepare("UPDATE tl_c4g_map_locstyles SET icon_size=?, icon_offset=? WHERE id =?")
+                    $this->Database->prepare('UPDATE tl_c4g_map_locstyles SET icon_size=?, icon_offset=? WHERE id =?')
                         ->execute($icon_size, $icon_offset, $id);
                 }
             }
@@ -77,22 +75,21 @@ class TlC4gMapLocstyles extends Backend
         if ($dc->activeRecord->styletype == 'cust_icon_svg') {
             $icon_src = $dc->activeRecord->svgSrc;
             $icon_size = $dc->activeRecord->icon_size;
-            if ($icon_src &! $icon_size) {
-                if (\Validator::isUuid($icon_src))
-                {
+            if ($icon_src & ! $icon_size) {
+                if (\Validator::isUuid($icon_src)) {
                     $iconSrc = \FilesModel::findByUuid($icon_src);
                     //list($width, $height, $type, $attr) = getimagesize($iconSrc->path);
                     $size = (new Contao\ImagineSvg\Imagine)->open($iconSrc->path)->getSize();
                     $width = $size->getWidth();
                     $height = $size->getHeight();
 
-                    $icon_size = 'a:3:{i:0;s:'.strlen($width).':"'.$width.'";i:1;s:'.strlen($height).':"'.$height.'";i:2;s:2:"px";}';
+                    $icon_size = 'a:3:{i:0;s:' . strlen($width) . ':"' . $width . '";i:1;s:' . strlen($height) . ':"' . $height . '";i:2;s:2:"px";}';
 
-                    $offsetWidth = '-'.($width / 2);
-                    $offsetHeight = '-'.($height / 2);
-                    $icon_offset = 'a:3:{i:0;s:'.strlen($offsetWidth).':"'.$offsetWidth.'";i:1;s:'.strlen($offsetHeight).':"'.$offsetHeight.'";i:2;s:2:"px";}';
+                    $offsetWidth = '-' . ($width / 2);
+                    $offsetHeight = '-' . ($height / 2);
+                    $icon_offset = 'a:3:{i:0;s:' . strlen($offsetWidth) . ':"' . $offsetWidth . '";i:1;s:' . strlen($offsetHeight) . ':"' . $offsetHeight . '";i:2;s:2:"px";}';
 
-                    $this->Database->prepare("UPDATE tl_c4g_map_locstyles SET icon_size=?, icon_offset=? WHERE id =?")
+                    $this->Database->prepare('UPDATE tl_c4g_map_locstyles SET icon_size=?, icon_offset=? WHERE id =?')
                         ->execute($icon_size, $icon_offset, $dc->id);
                 }
             }
